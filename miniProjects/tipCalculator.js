f9bfe7199b92cb54f033fff09a6b5ccd04a89a43
void main() <100292340+Webwizards60@users.noreply.github.com>
2023-01-11 16:34:41 +0530

    Mini Project Tip Calculator

--- miniProjects/tipCalculator.js
+++ miniProjects/tipCalculator.js
@@ -1,4 +1,14 @@
-const prompt = require("prompt-sync")();
+const prompt = require("prompt-sync")();    //defined prompt to take user input
 
-let name = prompt("please enter your name: ")
-console.log(name)
+let food = prompt("Price of the food: ")                        //price for the food
+let tip = prompt("How much tip you want to give (in %): ")      //total tip in %
+let totalTip = Number(food) * Number(tip) / 100                 //total tip on the food's price
+let finalBill = Number(food) + totalTip                         //total bill including tip
+console.log("Your total will be: ", finalBill,"$");
+
+/* the output will be like
+Price of the food: 2500 ⬅️
+How much tip you want to give (in %): 25 ⬅️
+
+Output: Your total will be: 3125$
+*/
